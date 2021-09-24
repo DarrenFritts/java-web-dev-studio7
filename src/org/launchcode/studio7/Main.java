bc5b1c194682d59ae6d598fa5b02e7bf8da8279c
DarrenFritts <84247704+DarrenFritts@users.noreply.github.com>
2021-09-23 20:51:34 -0500

    initial commit

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -6,6 +6,15 @@ public class Main {
 
         // TODO: Declare and initialize a CD and a DVD object.
 
+        CD myCD = new CD();
+        DVD myDVD = new DVD("My DVD");
+
+        myCD.spinDisc();
+        myDVD.spinDisc();
+
+        myCD.writeData("More data to write");
+        System.out.println(myCD.readData());
+
         // TODO: Call each CD and DVD method to verify that they work as expected.
 
     }
